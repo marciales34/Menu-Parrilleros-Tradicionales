06d7893232053e0050039fb76b485b7f1e2a2878
marciales34 <diegomarciales06@gmail.com>
2024-03-01 17:05:58 -0500

    Update

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import path from 'node:path';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+    {path: '', component:InicioComponent},
+    {path: 'login', component:LoginComponent}
+];
