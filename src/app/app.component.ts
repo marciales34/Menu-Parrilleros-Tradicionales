17de2a6972d109fe45d2e579a31b145a220601f2
marciales34 <diegomarciales06@gmail.com>
2024-03-08 01:23:19 -0500

    "upadate"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+import { Component, ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'MenuRestauranteParillaMaestra';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import path from 'node:path';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 
+
 export const routes: Routes = [
     {path: '', component:InicioComponent},
     {path: 'login', component:LoginComponent}
