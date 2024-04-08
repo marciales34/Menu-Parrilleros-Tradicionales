7a2df7f8feaf603323bf406544bc2683f2f2c80c
marciales34 <diegomarciales06@gmail.com>
2024-04-08 12:57:36 -0500

    "update"

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    const signUpButton = this.elementRef.nativeElement.querySelector("#signUp");
+    const signInButton = this.elementRef.nativeElement.querySelector("#signIn");
+    const container = this.elementRef.nativeElement.querySelector("#container");
+
+    signUpButton.addEventListener("click", () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener("click", () => {
+      container.classList.remove("right-panel-active");
+    });
+  }
 }
+
